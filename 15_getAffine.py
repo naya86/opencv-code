170a22b6506f838e1032fb1654f6b9db94b7b290
naya86 <80315799+naya86@users.noreply.github.com>
2021-04-24 00:09:37 +0900

    add

--- 15_getAffine.py
+++ 15_getAffine.py
@@ -3,18 +3,18 @@ import numpy as np
 
 # 원본이미지와 변환된 이미지 가지고, 변환에 사용된 행렬 구하기
 input_tri = np.float32( [ 50,50, 100,100, 200,150 ] )
-
-#삼각형 세 점 좌표로 변환.
+#삼각형 세 점 좌표로 변환. ( 2차원 )
 input_tri = input_tri.reshape(3,2)
-print(input_tri)
+
+
 #변환된 이미지의 세 점이 좌표
 ouput_tri = np.float32( [ 70,76, 142, 101, 272, 136 ] ) 
-
-#세점의 좌표로 변환
+#세점의 좌표로 변환 ( 2차원 )
 ouput_tri = ouput_tri.reshape(3,2)
 
 print(input_tri)
 print(ouput_tri)
+
 
 # 변환에 사용된 행렬 구하기.
 warpMat = cv2.getAffineTransform(input_tri,ouput_tri)
